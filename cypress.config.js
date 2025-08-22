af9ba103ac837fbc67126279bd70d69111a14452
sebastian <sebastian@boolean.cl>
2025-08-21 21:53:22 -0400

    Cypress on CI

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:1111",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://sbstn-jmnz.github.io/devops-fundamentals/')
+    cy.visit("/")
   })
 })
